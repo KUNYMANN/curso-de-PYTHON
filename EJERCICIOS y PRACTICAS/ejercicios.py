d0bb85d13747b153e3b815637d0095a47dafdfe3
KUNYMANN <149110752+KUNYMANN@users.noreply.github.com>
2024-06-06 21:15:26 -0300

    Update ejercicios.py

--- EJERCICIOS y PRACTICAS/ejercicios.py
+++ EJERCICIOS y PRACTICAS/ejercicios.py
@@ -6,3 +6,9 @@ cancion_de_cuna="Arroro mi Niño"
 artista_compositor=" de Joan Manuel Serrat " "El Catalan"
 print(cancion_de_cuna + artista_compositor)
 
+# IDENTAR significa dejar cuatro espacios con el espaciador o un tablulado desde el borde izquierdo 
+a=0
+for i in range(5): # los ":" significa que lo que sigue a continuacion pertenece al mismo bloque por eso aparece indentado
+    a+=1
+    print(a)
+
